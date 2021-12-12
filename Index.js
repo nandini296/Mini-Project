da1d735c7316218f863925c70d56787a7faa9043
nandini296 <88133813+nandini296@users.noreply.github.com>
2021-12-12 14:27:52 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -3,16 +3,11 @@ let nav = document.querySelector("nav");
 let ham = document.querySelector(".hamburger");
 console.log(navitem.classList);
 ham.addEventListener("click",()=>{
+
     ham.classList.toggle("active");
 
     const nav = document.querySelector("nav");
     nav.classList.toggle("open");
-    if(nav.classList.contains("open"))
-    {
-        nav.style.maxHeight = nav.scrollHeight + "px";
-    }
-    else{
-        nav.removeAttribute("style");
-    }
+    
 
 })
